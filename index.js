cbe2f361c09e8c14f400ab6ca7dac21f3d167b51
Nauticos <nathanlc008@gmail.com>
2026-06-24 15:45:59 +0100

    Added More Commands

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const { App } = require("@slack/bolt");
+const axios = require("axios");
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
@@ -25,10 +26,13 @@ app.command("/piggy-help", async ({ ack, respond }) => {
   await respond({
     text:
 `Available Commands:
+/piggy-help - Show this help message
 /piggy-ping - Check bot latency
 /piggy-translate - Translate text to Pig Latin
 /piggy-oink - 🐷
-/piggy-fiction - Pig Wikipedia`
+/piggy-fiction - Pig Wikipedia
+/piggy-joke - Get a joke
+/piggy-quote - Get a random quote from a pig`
   });
 });
 
@@ -71,4 +75,42 @@ app.command("/piggy-fiction", async ({ ack, respond }) => {
     text:
 `Have some famous pigs! https://en.wikipedia.org/wiki/List_of_fictional_pigs`
   });
+});
+
+app.command("/piggy-joke", async ({ ack, respond }) => {
+  await ack();
+
+  try {
+    const response = await axios.get("https://official-joke-api.appspot.com/random_joke");
+    await respond({
+      text:
+`${response.data.setup}
+
+${response.data.punchline}`
+    });
+  } catch (err) {
+    await respond({ text: "Failed to fetch a joke." });
+  }
+});
+
+app.command("/piggy-quote", async ({ ack, respond }) => {
+  await ack();
+    const QuoteNumber = Math.floor(Math.random() * 15)
+    const Quote = ["“It is deeply satisfying to know that my accomplishments are appreciated by a pig of your caliber.” - Wilbur (Charlotte's Web)",
+      "“All animals are equal, but some animals are more equal than others.” - Napoleon (Animal Farm)",
+      "“If you are jumping up and down in muddy puddles, you must wear your boots.” - Peppa Pig (Peppa Pig)",
+      "“I am an expert at lots of things.” - Daddy Pig (Peppa Pig)",
+      "“Th-th-th-that’s all, folks!” - Porky Pig (Looney Tunes)",
+      "“To the prosperity of The Manor Farm!” - Napoleon (Animal Farm)",
+      "“Four legs good, two legs bad.” - Snowball (Animal Farm)",
+      "“Well, I have to warn you, I may be small, but I can be ferocious if provoked.” - Babe (Babe: Pig in the City)",
+      "“I am fond of pigs. Dogs look up to us. Cats look down on us. Pigs treat us as equals.” - Winston Churchill",
+      "“I learned long ago, never to wrestle with a pig. You get dirty, and besides, the pig likes it.” - George Bernard Shaw",
+      "“Beauty is in the eye of the beholder and it may be necessary from time to time to give a stupid or misinformed beholder a black eye.” - Miss Piggy (The Muppet Show)",
+      "“They call me Mister Pig!” - Pumbaa (The Lion King)",
+      "“Hey, look, I'm Picasso!” - Hamm (Toy Story)",
+      "“Not by the hair on my chinny-chin-chin!” - The Pig (The Three Little Pigs)",];
+    const CurrentQuote = Quote[QuoteNumber];
+  await respond({
+     text: `${CurrentQuote}` });
 });
