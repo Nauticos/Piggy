e7208d628d2f4da30a9e1a2afea4610880e2217f
Nauticos <nathanlc008@gmail.com>
2026-06-24 13:56:03 +0100

    Added some commands

--- index.js
+++ index.js
@@ -12,10 +12,63 @@ app.command("/piggy-ping", async ({ command, ack, respond }) => {
   const start = Date.now();
   await ack();
   const latency = Date.now() - start;
-  await respond({ text: `Pong!\nLatency: ${latency}ms` });
+  await respond({ text: `Pong!\nLatency: ${latency}ms 🐷` });
 });
 
 (async () => {
   await app.start();
-  console.log("Bot is running!");
+  console.log("Bot is running! 🐷");
 })();
+
+app.command("/piggy-help", async ({ ack, respond }) => {
+  await ack();
+  await respond({
+    text:
+`Available Commands:
+/piggy-ping - Check bot latency
+/piggy-translate - Translate text to Pig Latin
+/piggy-oink - 🐷
+/piggy-fiction - Pig Wikipedia`
+  });
+});
+
+app.command("/piggy-oink", async ({ ack, respond }) => {
+  await ack();
+  await respond({
+    text:
+`Oink! Oink! 🐷`
+  });
+});
+
+app.command("/piggy-translate", async ({ command, ack, respond }) => {
+    await ack();
+    function translateToPigLatin(text) {
+        const words = text.split(/\s+/);
+        const translated = words.map(word => {
+            const vowelMatch = word.match(/[aeiou]/i);
+            if(!vowelMatch) return word;
+            const firstVowel = word.indexOf(vowelMatch[0])
+
+            if (firstVowel === 0) {
+                return word + "way";
+            } else {
+                const consonants = word.slice(0, firstVowel);
+                const restOfWord = word.slice(firstVowel);
+                return restOfWord + consonants + "ay";
+            }
+            });
+        return translated.join(" ");
+    }
+    const translated = translateToPigLatin(command.text);
+    await respond({
+        text: `${translated} 🐷`
+    });
+});
+
+app.command("/piggy-fiction", async ({ ack, respond }) => {
+  await ack();
+  await respond({
+    text:
+`Have some famous pigs! https://en.wikipedia.org/wiki/List_of_fictional_pigs`
+  });
+});
